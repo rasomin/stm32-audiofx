37b27f986e233ef8ca67b8199329339ccd3f308c
Cheolu Choi <ray@rasom.in>
2025-01-31 01:45:54 +0900

    feat: gain 구현 및 이펙트 함수화

--- lib/audio-effects/src/main.rs
+++ lib/audio-effects/src/main.rs
@@ -11,18 +11,12 @@ fn main() {
 
     while let Some(frame) = decoder.next() {
         if let Frame::Audio(audio) = frame {
-            let delay_samples = audio.sample_rate() / 30; // 잔향
             let samples = audio.samples();
             sample_rate = audio.sample_rate();
 
-            for i in 0..samples.len() {
-                if i >= delay_samples as usize {
-                    let new_sample = samples[i] + samples[i - (delay_samples as usize)] / 2;
-                    effected_samples.push(new_sample);
-                } else {
-                    effected_samples.push(samples[i]);
-                }
-            }
+            // let mut effected = effect_reverb(&samples, sample_rate);
+            let mut effected = effect_gain(&samples, 5.5);
+            effected_samples.append(&mut effected);
         }
     }
 
@@ -44,4 +38,31 @@ fn save_wav(filename: &str, samples: &[i16], sample_rate: u32) {
     }
 
     writer.finalize().expect("Failed to finalize WAV file");
+}
+
+fn effect_gain(samples: &[i16], gain: f32) -> Vec<i16> {
+    let mut effected_samples_boost: Vec<i16> = Vec::new();
+
+    for &sample in samples {
+        let new_sample = ((sample as f32) * gain) as i16;
+        effected_samples_boost.push(new_sample);
+    }
+
+    return effected_samples_boost;
+}
+
+fn _effect_reverb(samples: &[i16], sample_rate: u32) -> Vec<i16> { // doesn't work
+    let delay_samples = sample_rate / 30;
+    let mut effected_samples_reverb: Vec<i16> = Vec::new();
+
+    for i in 0..samples.len() {
+        if i >= delay_samples as usize {
+            let new_sample = samples[i] + samples[i - (delay_samples as usize)] / 2;
+            effected_samples_reverb.push(new_sample);
+        } else {
+            effected_samples_reverb.push(samples[i]);
+        }
+    }
+
+    return effected_samples_reverb;
 }
